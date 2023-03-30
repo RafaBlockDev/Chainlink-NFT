47fb5fa55d46f9c82d92c806dc649176ae2faf75
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-29 19:42:28 -0600

    config: add dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
