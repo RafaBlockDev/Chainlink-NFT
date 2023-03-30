7893c1af84c17992d51e7841362d5c55ffc001e4
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-29 21:05:33 -0600

    NFT Collection (#2)
    * package: add OZ
    
    * config: add var env
    
    * config: add hardat config
    
    * config: add dotenv
    
    * contract: end the smart contract of collection

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
